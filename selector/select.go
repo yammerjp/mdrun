f63a8b74b62edeb3a40bfda5a8a371307f03c63c
Keisuke Nakayama <me@yammer.jp>
2023-02-26 06:47:38 +0000

    Add preview

--- selector/select.go
+++ selector/select.go
@@ -3,22 +3,30 @@ package selector
 import (
 	"fmt"
 	"os"
-	"github.com/yammerjp/mdrun/markdown"
-	fizzyfinder "github.com/ktr0731/go-fuzzyfinder"
 
+	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/yammerjp/mdrun/markdown"
 )
 
 func Select(commandBlocks []markdown.CommandBlock) markdown.CommandBlock {
-		idx, err := fizzyfinder.Find(
-			commandBlocks,
-			func(i int) string {
-				cb := commandBlocks[i]
-				return cb.CommandOneLineString() + " # " + cb.Title
-			})
-		if err != nil {
-			fmt.Println("Error occured")
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		return commandBlocks[idx]
+	idx, err := fuzzyfinder.Find(
+		commandBlocks,
+		func(i int) string {
+			cb := commandBlocks[i]
+			return cb.CommandOneLineString() + " # " + cb.Title
+		},
+		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			return fmt.Sprintf(
+				"%s\n---\n\n```\n%s\n```",
+				commandBlocks[i].Title,
+				commandBlocks[i].Command,
+			)
+		}),
+	)
+	if err != nil {
+		fmt.Println("Error occured")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return commandBlocks[idx]
 }
